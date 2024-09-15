a1f23a7c516ecaa3465829967599b552b7a48cef
matthew-jang <matthew.jang@eagles.oc.edu>
2024-09-14 21:43:57 -0500

    landing image for ai

--- js/matthew.js
+++ js/matthew.js
@@ -10,13 +10,11 @@ function changeGetingStartedTitle() {
     // Update the index to point to the next emoji
     titleIndex = (titleIndex + 1) % titles.length;
 }
-
-// Change emoji every 2 seconds (2000 milliseconds)
-setInterval(changeGetingStartedTitle, 1300);
+setInterval(changeGetingStartedTitle, 1300); // Change emoji every 2 seconds (2000 milliseconds)
 
 
 
-/********************************************************* */
+/*************************************************************************************************************** */
 // SOFTWARE DEV PAGE
 const dev_titles = ["Software Development", "Collaborations", "Reviews", "Innovations"];
 let dev_titlesIndex = 0;
@@ -29,12 +27,11 @@ function changeSoftwareDevTitle() {
     // Update the index to point to the next emoji
     dev_titlesIndex = (dev_titlesIndex + 1) % dev_titles.length;
 }
-
-// Change emoji every 2 seconds (2000 milliseconds)
-setInterval(changeSoftwareDevTitle, 1300);
+setInterval(changeGetingStartedTitle, 1300); // Change emoji every 2 seconds (2000 milliseconds)
 
 
-/********************************************************* */
+
+/*************************************************************************************************************** */
 // CYBERSECURITY PAGE
 const cyber_titles = ["Cybersecurity", "Hacking", "Protecting", "Analysis"];
 let cyber_titlesIndex = 0;
@@ -47,12 +44,28 @@ function changeSoftwareDevTitle() {
     // Update the index to point to the next emoji
     cyber_titlesIndex = (cyber_titlesIndex + 1) % cyber_titles.length;
 }
-
-// Change emoji every 2 seconds (2000 milliseconds)
-setInterval(changeSoftwareDevTitle, 1300);
+setInterval(changeGetingStartedTitle, 1300); // Change emoji every 2 seconds (2000 milliseconds)
 
 
-/********************************************************* */
+
+/*************************************************************************************************************** */
+// AI PAGE
+const ai_titles = ["AI", "Automation", "Algorithms", "Research"];
+let ai_titlesIndex = 0;
+
+// Function to change the emoji
+function changeAiTitle() {
+    const container = document.getElementById('ai-title-container');
+    // Update the content of the container with the current emoji
+    container.textContent = ai_titles[ai_titlesIndex];
+    // Update the index to point to the next emoji
+    ai_titlesIndex = (ai_titlesIndex + 1) % ai_titles.length;
+}
+setInterval(changeAiTitle, 1300); // Change emoji every 2 seconds (2000 milliseconds)
+
+
+
+/*************************************************************************************************************** */
 // A DAY IN THE LIFE PAGE
 const day_titles = ["Collaborating", "Reviewing", "Problem Solving", "Debugging", "Coding"];
 let day_titlesIndex = 0;
@@ -65,7 +78,5 @@ function changDayInTheLifeTitle() {
     // Update the index to point to the next emoji
     day_titlesIndex = (day_titlesIndex + 1) % day_titles.length;
 }
-
-// Change emoji every 2 seconds (2000 milliseconds)
-setInterval(changDayInTheLifeTitle, 1300);
+setInterval(changeGetingStartedTitle, 1300); // Change emoji every 2 seconds (2000 milliseconds)
 
